87ddd86753b70a59274e5d87f2f080eb55c470a5
Robert Love <terp4life2001@gmail.com>
2021-03-31 20:20:25 -0400

    more-imps-rob

--- src/cutVideo.py
+++ src/cutVideo.py
@@ -21,11 +21,51 @@ end_time = args.end_time
 prefix = args.prefix
 target_dir = args.target_dir
 
+if not os.path.isdir(target_dir):
+    print(target_dir + "is not a valid directory.")
+    sys.exit(os.EX_OSFILE)
+
+if not os.path.isfile(in_file):
+    print(in_file + " is not a valid file.")
+    sys.exit(os.EX_OSFILE)
+
+def findNextNumberOutfile(f):
+    import re
+
+    print("Checking if \'" + f + "\' is a file")
+    if not os.path.isfile(f):
+        print("\'" + f + "\' is not a file")
+        print("returning: " + f)
+        return f
+
+    m = re.search(r'(?:[/ ]){prefix}-(\d+)', f)
+    if not m:
+        print("Could not find a number")
+        print("returning: " + f)
+        return f
+
+    print("Number is " + str(m) + ".")
+    print(m.group(1))
+    num = int(m.group(1))
+    num += 1
+    f2 = target_dir + prefix + "-" + str(num) + "." + in_name
+
+    print("Checking if \'" + f2 + "\' is a file")
+    if not os.path.isfile(f2):
+        print("\'" + f2 + "\' is not a file")
+        print("returning: " + f2)
+        return f2
+    else:
+        return findNextNumberOutfile(f2)
+
 in_name = os.path.basename(in_file)
 
 out_file = target_dir + "/" + prefix + in_name
 if os.path.isfile(out_file):
     out_file = target_dir + "/" + prefix + "-2." + in_name
+
+f = "/mnt/vmheart/Download/Porn/" + prefix + "-1." + in_name
+out_file = findNextNumberOutfile(f)
 
 print("out_file: " + out_file)
 
