bc7903312f8d9644d07146eb4740bae40fce3a97
Robert Love <terp4life2001@gmail.com>
2022-05-07 11:54:15 -0400

    fix-build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-buildah build --format=docker -f Dockerfile -t brain:5000/cutVideo .
+buildah build --format=docker -f Dockerfile -t brain:5000/cutvideo .
 
 buildah push --tls-verify=false brain:5000/cutvideo:latest
